6c1fc362d1e7d7b4a264f4e95b6a14fdd01827a6
Shiv Bharthur <shiv.bharthur@gmail.com>
2011-09-29 11:35:14 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClothStore::Application.config.secret_token = 'a1c0ea2a6b7d66ba7435693099c573ce8887e29537bdcaa1486ee7b3e0ea7b27f9fe214b1c419881d2e935bfa378d9a5ca18baf6f71bb922d3547b4179138436'
